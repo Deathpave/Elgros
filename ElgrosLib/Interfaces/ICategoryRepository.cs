956dbae16206acd1918c23615dd02cade132f7f2
Deathpave <49186768+Deathpave@users.noreply.github.com>
2023-04-14 11:07:47 +0200

    Added categories

---
+++ ElgrosLib/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,46 @@
+﻿using ElgrosLib.DataModels;
+
+namespace ElgrosLib.Interfaces
+{
+    internal interface ICategoryRepository
+    {
+        /// <summary>
+        /// Adds a new category to the database
+        /// </summary>
+        /// <param name="createEntity"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<bool> CreateAsync(Product createEntity);
+
+        /// <summary>
+        /// Deletes a category from the database
+        /// </summary>
+        /// <param name="deleteEntity"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<bool> DeleteAsync(Product deleteEntity);
+
+        /// <summary>
+        /// Gets all categorys
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<IEnumerable<Product>> GetAllAsync();
+
+        /// <summary>
+        /// Gets a category by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<Product> GetByIdAsync(long id);
+
+        /// <summary>
+        /// Updates a category
+        /// </summary>
+        /// <param name="updateEntity"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<bool> UpdateAsync(Product updateEntity);
+    }
+}
