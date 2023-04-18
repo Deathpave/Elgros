8967107a23b0171189701cc1975ab7eb4b338ad0
Deathpave <49186768+Deathpave@users.noreply.github.com>
2023-04-18 09:26:17 +0200

    Added encryption and decryption

---
+++ ElgrosLib/Security/Decryption.cs
@@ -0,0 +1,47 @@
+﻿using ElgrosLib.Factories;
+using ElgrosLib.Logs;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ElgrosLib.Security
+{
+    internal class Decryption
+    {
+        public string DecryptString(string input, string encodingPassword)
+        {
+            // Check for null or empty inputs
+            if (string.IsNullOrEmpty(input))
+            {
+                LogFactory.CreateLog(LogTypes.Console, "Input string was null or empty", MessageTypes.Error).WriteLog();
+            }
+            if (string.IsNullOrEmpty(encodingPassword))
+            {
+                LogFactory.CreateLog(LogTypes.Console, "Encoding password was null or empty", MessageTypes.Error).WriteLog();
+            }
+
+            // input bytes as salt
+            byte[] salt = Encoding.UTF8.GetBytes(input);
+            // Key and vector generator
+            Rfc2898DeriveBytes rfc = new Rfc2898DeriveBytes(encodingPassword, salt);
+
+            // Create the Aes for encryptin
+            Aes aes = Aes.Create();
+            aes.Key = rfc.GetBytes(32);
+            aes.IV = rfc.GetBytes(16);
+
+            // Create streams for encryption
+            MemoryStream memoryStream = new MemoryStream();
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, aes.CreateDecryptor(), CryptoStreamMode.Write);
+
+            // Encrypt input as bytes
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            cryptoStream.Write(inputBytes, 0, inputBytes.Length);
+            cryptoStream.FlushFinalBlock();
+            cryptoStream.Close();
+
+            // Convert encrypted bytes to base64 string
+            string result = Convert.ToBase64String(memoryStream.ToArray());
+            return result;
+        }
+    }
+}
