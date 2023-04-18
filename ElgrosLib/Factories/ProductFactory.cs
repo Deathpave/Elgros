daadb9016afd7077decf120f3acf7ca0ad3d157c
Deathpave <49186768+Deathpave@users.noreply.github.com>
2023-04-18 11:19:19 +0200

    product factory

--- ElgrosLib/Factories/ProductFactory.cs
+++ ElgrosLib/Factories/ProductFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ElgrosLib.DataModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,20 @@ using System.Threading.Tasks;
 
 namespace ElgrosLib.Factories
 {
-    public class ProductFactory
+
+    /// <summary>
+    /// Factory that handles the creation of product objects
+    /// </summary>
+    public static class ProductFactory
     {
+        /// <summary>
+        /// Create product instance
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static Product CreateProduct(string name, string description, double price, double quantity, string photoUrl, int categoryId, int subCategoryId)
+        {
+            return new Product(0, name, description, price, quantity, photoUrl, categoryId, subCategoryId);
+        }
     }
 }
