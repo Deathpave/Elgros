b07b641dd3934f65e37fa9b096c0a65674577146
Deathpave <49186768+Deathpave@users.noreply.github.com>
2023-04-18 11:20:04 +0200

    usings

--- ElgrosLib/Factories/ProductFactory.cs
+++ ElgrosLib/Factories/ProductFactory.cs
@@ -1,9 +1,4 @@
 ﻿using ElgrosLib.DataModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ElgrosLib.Factories
 {
