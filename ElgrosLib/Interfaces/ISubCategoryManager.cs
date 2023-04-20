0a75c5ab86b50eb346995a92d3d92473067d89b8
JesperKD <Jesperd55@gmail.com>
2023-04-20 12:43:39 +0200

    Revert "Revert "Revert "IManager update"""
    This reverts commit a9dfc6504e708aa77a1ac70564fcc445a1fcc0cb.

--- ElgrosLib/Interfaces/ISubCategoryManager.cs
+++ ElgrosLib/Interfaces/ISubCategoryManager.cs
@@ -1,9 +1,51 @@
 ﻿using ElgrosLib.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ElgrosLib.Interfaces
 {
-    public interface ISubCategoryManager : IManager<SubCategory>
+    public interface ISubCategoryManager
     {
+        /// <summary>
+        /// Adds a new subcategory to the database
+        /// </summary>
+        /// <param name="createEntity"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<bool> CreateAsync(SubCategory createEntity);
 
+        /// <summary>
+        /// Deletes a subcategory from the database
+        /// </summary>
+        /// <param name="deleteEntity"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<bool> DeleteAsync(SubCategory deleteEntity);
+
+        /// <summary>
+        /// Gets all subcategories
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<IEnumerable<SubCategory>> GetAllAsync();
+
+        /// <summary>
+        /// Gets a subcategory by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<SubCategory> GetByIdAsync(int id);
+
+        /// <summary>
+        /// Updates a subcategory
+        /// </summary>
+        /// <param name="updateEntity"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<bool> UpdateAsync(SubCategory updateEntity);
     }
 }
--- ElgrosLib/Interfaces/IUserManager.cs
+++ ElgrosLib/Interfaces/IUserManager.cs
@@ -2,8 +2,47 @@
 
 namespace ElgrosLib.Interfaces
 {
-    public interface IUserManager : IManager<User>
+    public interface IUserManager
     {
         public Task<int> CheckLogin(string username, string password);
+
+        /// <summary>
+        /// Adds a new user to the database
+        /// </summary>
+        /// <param name="createEntity"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<bool> CreateAsync(User createEntity);
+
+        /// <summary>
+        /// Deletes a user from the database
+        /// </summary>
+        /// <param name="deleteEntity"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<bool> DeleteAsync(User deleteEntity);
+
+        /// <summary>
+        /// Gets all users
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<IEnumerable<User>> GetAllAsync();
+
+        /// <summary>
+        /// Gets a user by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<User> GetByIdAsync(int id);
+
+        /// <summary>
+        /// Updates a user
+        /// </summary>
+        /// <param name="updateEntity"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<bool> UpdateAsync(User updateEntity);
     }
 }
