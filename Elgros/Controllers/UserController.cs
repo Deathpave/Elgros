4ec2521b2e3092540708cd7909febd6cf2dd2fdf
Olivervesth <Olivervesth>
2023-04-20 10:24:05 +0200

    added session

---
+++ Elgros/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Elgros.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet("/cart")]
+        public async Task<IActionResult> Cart()
+        {
+
+            return View("Cart");
+        }
+    }
+}
